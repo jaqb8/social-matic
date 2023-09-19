7896e060b91d7553f4b55a02a6caf451c84e9e52
jaqb8 <qba.anisz@gmail.com>
2023-09-19 11:43:19 +0200

    dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,133 @@
+import { useUser } from "@clerk/nextjs";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { type RouterOutputs, api } from "~/utils/api";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+
+const Sidebar = () => {
+  const { user, isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return null;
+  }
+
+  return (
+    <div className="w-72 p-10">
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-1">
+          <Image
+            src={user.imageUrl}
+            alt="Profile image"
+            width={80}
+            height={80}
+            className="rounded-2xl"
+          />
+          <h1 className="text-2xl font-bold">{user.username}</h1>
+          <p className="text-sm text-secondary">
+            {user.primaryEmailAddress?.emailAddress}
+          </p>
+        </div>
+        <div>
+          <li className="flex list-none flex-col gap-3 font-bold text-secondary">
+            <ul className="cursor-pointer hover:text-primary">
+              <Link href="/dashboard">Dashboard</Link>
+            </ul>
+            <ul className="cursor-pointer hover:text-primary">Archive</ul>
+            <ul className="cursor-pointer hover:text-primary">Settings</ul>
+            <ul className="cursor-pointer hover:text-primary">Account</ul>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+type PostWithAuthor = RouterOutputs["posts"]["getAll"][number];
+
+const PostView = (props: PostWithAuthor) => {
+  const { post, author } = props;
+
+  const postIcon = {
+    TWITTER: "/x.png",
+    LINKEDIN: "/linkedin.png",
+  };
+
+  return (
+    <div
+      className="flex gap-3 overflow-hidden break-words px-2 py-4"
+      key={post.id}
+    >
+      <div>
+        <Image
+          src={postIcon[post.platform]}
+          width={50}
+          height={50}
+          alt="Author profile image"
+          className="rounded-full"
+        />
+      </div>
+      <div className="block">
+        <h3>{post.content}</h3>
+        <p className="font-thin ">
+          {`${dayjs(post.postDate).fromNow()} (${dayjs(post.postDate).format(
+            "DD/MM/YYYY HH:mm",
+          )})`}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const RightContent = () => {
+  const { data, isLoading } = api.posts.getAll.useQuery();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="grow bg-slate-300 px-14 py-8">
+      {!data && <div>Something went wrong...</div>}
+      <h1 className="text-lg font-bold">Scheduled posts</h1>
+      {!!data &&
+        data.map((fullPost) => (
+          <PostView key={fullPost.post.id} {...fullPost} />
+        ))}
+    </section>
+  );
+};
+
+const Content = () => {
+  return (
+    <div className="flex w-full flex-col overflow-hidden rounded-2xl text-slate-900 xl:flex-row">
+      <section id="main-content" className="bg-slate-100 px-14 py-8 xl:w-[70%]">
+        <div className="flex flex-col gap-8">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <textarea className="rounded p-4" cols={20} rows={5} />
+          <div className="flex justify-end gap-3 text-sm">
+            <button className="btn btn-primary">Post now</button>
+            <button className="btn btn-primary">Schedule post</button>
+          </div>
+        </div>
+      </section>
+      <RightContent />
+    </div>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <main className="h-screen p-8">
+      <div className="flex gap-8">
+        <Sidebar />
+        <Content />
+      </div>
+    </main>
+  );
+};
+
+export default Dashboard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f1f5f9",
+        secondary: "#9ca3af",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
